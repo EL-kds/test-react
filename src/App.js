81b7c7372db3271bf7da96fd56884d49b3f0b14d
el-kds <wright.kds96@gmail.com>
2024-10-01 11:47:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import NavBar from "./components/Navbar";
+import Hero from "./components/hero";
+import Footer from "./components/footer";
+import Services from "./components/services";
+import Tours from "./components/tours"
+import About from "./components/about"
+import './App.css';
+
+function App() {
+  console.log('hello')
+  return (
+    <>
+    <NavBar/>
+    <Hero/>
+    <About/>
+    <Services/>
+    <Tours/>
+    <Footer/>
+    
+    
+    
+      
+
+   
+
+    
+    
+
+    
+
+    
+    </>
+  );
+}
+
+export default App;
